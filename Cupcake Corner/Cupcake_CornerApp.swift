803d84a9405a3c46367721169c70f928251f4d7f
Khaled Hossameldin <khaled-hossam@outlook.com>
2024-08-23 18:12:57 +0300

    Initial Commit

---
+++ Cupcake Corner/Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_CornerApp.swift
+//  Cupcake Corner
+//
+//  Created by Khaled Hossameldin on 23/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
