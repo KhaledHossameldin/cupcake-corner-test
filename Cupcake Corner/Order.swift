6434ea49f32eb83e9a0296fc4bd802caecb5dda6
Khaled Hossameldin <khaled-hossam@outlook.com>
2024-08-29 12:18:28 +0300

    initial commit

---
+++ Cupcake Corner/Order.swift
@@ -0,0 +1,71 @@
+//
+//  Order.swift
+//  Cupcake Corner
+//
+//  Created by Khaled Hossameldin on 27/08/2024.
+//
+
+import Foundation
+
+@Observable
+class Order: Codable {
+    enum CodingKeys: String, CodingKey {
+        case _type = "type"
+        case _quantity = "quantity"
+        case _specialRequestEnabled = "specialRequestEnabled"
+        case _extraFroasting = "extraFroasting"
+        case _addSprinkles = "addSprinkles"
+        case _name = "name"
+        case _streetAddress = "streetAddress"
+        case _city = "city"
+        case _zip = "zip"
+    }
+    
+    static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
+    
+    var type = 0
+    var quantity = 3
+    
+    var specialRequestEnabled = false {
+        didSet {
+            if specialRequestEnabled == false {
+                extraFroasting = false
+                addSprinkles = false
+            }
+        }
+    }
+    var extraFroasting = false
+    var addSprinkles = false
+    
+    var name = ""
+    var streetAddress = ""
+    var city = ""
+    var zip = ""
+    
+    var hasValidAddress: Bool {
+        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+            return false;
+        }
+        return true;
+    }
+    
+    var cost: Decimal {
+        // $2 per cake
+        var cost = Decimal(quantity) * 2
+        
+        // complicated cakes cost more
+        cost += Decimal(type) / 2
+        
+        // $1/cake for extra frosting
+        if extraFroasting {
+            cost += Decimal(quantity)
+        }
+        
+        // $0.50/cake for sprinkles
+        if addSprinkles {
+            cost += Decimal(quantity)
+        }
+        
+        return cost
+    }
+}
